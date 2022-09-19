f6fe5661f9f6bdadf536a7110239623d524bcd00
Rimcas <eguducasidi0@gmail.com>
2022-09-19 07:32:42 -0700

    dockfile4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,12 @@ int count = 0;
 
 while (count >= 0)
 {
-if (s[count] == 0)
+if (s[count] == '\0')
 break;
 count++;
 }
 
 for (count--; count >= 0; count--)
 _putchar(s[count]);
-_putchar(n);
+_putchar('\n');
 }
