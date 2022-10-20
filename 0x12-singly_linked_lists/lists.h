f6b7ca85a7ba13e8cd87495588592b75a0c39825
Rimcas <eguducasidi0@gmail.com>
2022-10-20 15:50:13 -0700

    file head

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,13 @@
+/**
+  * struct list_s - singly linked list
+  * @str: string - (malloc'ed string)
+  * @len: length of the string
+  * @next: points to the next node
+  *
+  * Description: singly linked list node structure
+  *              for ALX project
+  */
+
 typedef struct list_s
 {
 	char *str;
