c57a4c797ba07765c227a56180cd4da06870d0de
Rimcas <eguducasidi0@gmail.com>
2022-10-20 18:51:03 -0700

    checkedfile

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: takes an integer
+ * Description: the program takes an integer
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
 	int a[5];
 	int *p;
+	int n;
 
 	a[2] = 1024;
 	p = &n;
